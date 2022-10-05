a66294e683ecf5910adc214a80c0fbb5465bdca3
sy9612 <sy9612@naver.com>
2022-10-05 22:26:55 +0900

    Time: 3 ms (97.93%), Space: 52.5 MB (92.72%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int answer = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while(left < right){
+            answer = Math.max(answer, (right - left) * Math.min(height[right], height[left]));
+
+            
+            if(height[left] <= height[right]){
+                left++;
+            }else
+                right--;
+            
+            //System.out.println(answer + " " + (right - left) + " " + Math.min(height[right], height[left]));
+
+        }
+        
+        
+        return answer;
+    }
+}
